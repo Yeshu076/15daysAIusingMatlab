12e0936b21ffff6016940fc031ecfe9384151ade
Yeshwanth.J <yeshwanth.yeshu07@gmail.com>
2021-02-08 10:37:47 +0530

    Day 3

--- Day3/ex5.m
+++
@@ -1,8 +0,0 @@
-clc;
-
-businessCard = imread('businessCard.png');
- ocrResults   = ocr(businessCard);
- bboxes = locateText(ocrResults, 'MathWorks', 'IgnoreCase', true);
- Iocr   = insertShape(businessCard, 'FilledRectangle', bboxes);
- figure
- imshow(businessCard)
